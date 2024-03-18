0287a808cfc8c30eb224b854ec4c148709b6ba29
NTaskova <ntaskova@intracol.com>
2024-03-18 11:48:21 +0200

    rename controller package

--- src/main/java/com/jpa/demo/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jpa.demo.repository;
-
-import com.jpa.demo.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Product findByName(String name);
-}
